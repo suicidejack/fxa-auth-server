95a143b6d254f9c37e1ded40d355ea55a0ca6522
matt price <mattprice11@gmail.com>
2014-09-11 03:05:07 +0000

    dont use inmemory db on start

--- scripts/start-local.sh
+++ scripts/start-local.sh
@@ -2,7 +2,7 @@
 node ./scripts/gen_keys.js
 node ./test/mail_helper.js &
 MH=$!
-node ./node_modules/fxa-auth-db-mem &
+#node ./node_modules/fxa-auth-db-mem &
 DB=$!
 
 node ./bin/key_server.js | node ./bin/notifier.js >/dev/null
